e1f732e5193f87913fee1510059c50cd4a023ac0
tgtg <617906617@qq.com>
2025-12-25 22:55:21 +0800

    update src file from pakeplus

---
+++ scripts/www/constants.ts
@@ -0,0 +1,33 @@
+
+export const COLORS = {
+  GOLD: '#d4af37',
+  WINE: '#722f37',
+  PALE_BLUE: '#a8c3d4',
+  ROSE_PINK: '#e6a8d7',
+  CHAMPAGNE: '#fceea7',
+  WHITE: '#ffffff',
+  DEEP_GREEN: '#1b4d3e',
+  ORANGE: '#ff8c00',
+  NAVY_BLUE: '#000080',
+  CANDY_RED: '#ff0000',
+  RETRO_GOLD: '#bf9b30',
+  WINE_RED: '#8b0000',
+  GRAY_BLUE: '#5d7d9a',
+  CHAMPAGNE_GOLD: '#f7e7ce'
+};
+
+export const TREE_PARTICLE_COUNT = 2000;
+export const ORNAMENT_COUNT = 500;
+export const EXTRA_ORNAMENT_COUNT = 1500;
+export const PARTICLE_COUNT = TREE_PARTICLE_COUNT + ORNAMENT_COUNT + EXTRA_ORNAMENT_COUNT;
+
+export const DUST_COUNT = 3000;
+export const MAX_RADIUS = 12;
+export const TREE_HEIGHT = 28;
+
+export const GESTURES = {
+  OPEN_PALM: 'Open Palm',
+  CLOSED_FIST: 'Closed Fist',
+  PINCH: 'Pinch',
+  NONE: 'None'
+};
